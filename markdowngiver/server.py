9085188f758e603b54a682d29b7c091fc31c2602
ThePresistence <102717267+ThePresistence@users.noreply.github.com>
2025-04-11 03:37:22 +0000

    added more

--- markdowngiver/server.py
+++ markdowngiver/server.py
@@ -39,6 +39,7 @@ if __name__ == "__main__":
 #                 "mcp[cli]", 
 #                 "mcp", 
 #                 "run", 
+#                 "-m",
 #                 "<pura-path>"
 #             ]
 #         }
